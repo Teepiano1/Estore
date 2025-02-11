d4e69918a79df953732ec08f4fa30a91789d905b
teeTech <127060461+Teepiano1@users.noreply.github.com>
2025-02-11 11:39:38 +0100

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,30 @@
-import { React, useState} from "react";
+import { React, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Home from "./pages/Home/Home";
-import Footer from "./components/footer/footer";
+import Footer from "./components/footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 import Verify from "./pages/Verify/Verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
 
 const App = () => {
-  const [showLogin, setshowLogin] = useState(false)
+  const [showLogin, setshowLogin] = useState(false);
   return (
     <>
-    {showLogin?<LoginPopup setshowLogin={setshowLogin}/>:<></>}
+      {showLogin ? <LoginPopup setshowLogin={setshowLogin} /> : <></>}
       <div className="app">
         <Navbar setshowLogin={setshowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
-          <Route path="/verify" element={<Verify/>}></Route>
-          <Route path="/myorders" element={<MyOrders/>}></Route>
+          <Route path="/verify" element={<Verify />}></Route>
+          <Route path="/myorders" element={<MyOrders />}></Route>
         </Routes>
       </div>
-      |<Footer />
+      <Footer />
     </>
   );
 };
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -8,29 +8,34 @@ const footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis iure quas, accusamus quam voluptatibus itaque possimus sunt cum consectetur fuga facilis eum, ipsa ipsum dolores! Impedit, voluptatibus? Omnis, vero. Laudantium.</p>
-            <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
-            </div>
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Perferendis iure quas, accusamus quam voluptatibus itaque possimus
+            sunt cum consectetur fuga facilis eum, ipsa ipsum dolores! Impedit,
+            voluptatibus? Omnis, vero. Laudantium.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
         <div className="footer-content-center">
-            <h2>COMPANY</h2>
-            <ul>
-                <a href="#navbar">Home</a>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
-            </ul>
+          <h2>COMPANY</h2>
+          <ul>
+            <a href="#navbar">Home</a>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
         </div>
         <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-                <li>+2348077784207</li>
-                <li>contact@tomato.com</li>
-            </ul>
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+2348077784207</li>
+            <li>contact@tomato.com</li>
+          </ul>
         </div>
       </div>
       <hr />
